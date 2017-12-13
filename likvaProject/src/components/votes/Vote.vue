31730c59c4febb19d2773278d3a75eac85f644ac
LeoMouyna <mouynaleo@gmail.com>
2017-12-13 23:19:46 +0100

    Change route and vote component

--- likvaProject/src/components/votes/Vote.vue
+++ likvaProject/src/components/votes/Vote.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="vote">
     <div class="btn-group">
-      <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton"
+      <button class="btn btn-primary dropdown-toggle btn-lg" type="button" id="dropdownMenuButton"
               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Votre Choix
       </button>
@@ -10,8 +10,8 @@
         <a class="dropdown-item" href="#">Another action</a>
         <a class="dropdown-item" href="#">Something else here</a>
       </div>
-      <button type="button" class="btn btn-primary">S'abstenir</button>
-      <button type="button" class="btn btn-primary">Sony</button>
+      <button type="button" class="btn btn-primary btn-lg">S'abstenir</button>
+      <button type="button" class="btn btn-primary btn-lg">Sony</button>
     </div>
   </div>
 </template>
@@ -29,5 +29,12 @@
 </script>
 
 <style scoped>
-
+  .vote{
+    position: fixed;
+    bottom: 5%;
+    z-index: 12;
+    width: 100%;
+    padding-left: 15%;
+    padding-right: 15%;
+  }
 </style>
