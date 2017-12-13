814a560ac3ba4c8dfbf78cc3b72329c7fa25b4ca
LeoMouyna <mouynaleo@gmail.com>
2017-12-13 22:04:57 +0100

    Add console printing in proposition display

--- likvaProject/src/components/propositions/PropositionDisplay.vue
+++ likvaProject/src/components/propositions/PropositionDisplay.vue
@@ -48,7 +48,8 @@
       this.propositionResource = this.$resource('http://127.0.0.1:3000/api/teams{/slug}/propositions{/idProposition}')
       this.propositionResource.get(
         {slug: this.slug, idProposition: this.idProposition}).then(response => {
-          this.proposition = response.body.props
+        this.proposition = response.body.props
+        console.log('La proposition: ' + this.proposition)
         })
     }
   }
