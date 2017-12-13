b05683559323b97785fd435b396c6c616b19a74d
LeoMouyna <mouynaleo@gmail.com>
2017-12-13 21:28:09 +0100

    Add params for slug and Id

--- likvaProject/src/components/propositions/PropositionDisplay.vue
+++ likvaProject/src/components/propositions/PropositionDisplay.vue
@@ -38,11 +38,11 @@
       ])
     },
     mounted () {
-      this.slug = this.$router.history.current.slug
-      this.idProposition = this.$router.history.current.idProposition
+      this.slug = this.$router.history.current.params.slug
+      this.idProposition = this.$router.history.current.params.idProposition
       console.log('Routeur: ' + this.$router)
-      console.log('Slug: ' + this.$router.history.current.slug)
-      console.log('Id: ' + this.$router.history.current.idProposition)
+      console.log('Slug: ' + this.$router.history.current.params.slug)
+      console.log('Id: ' + this.$router.history.current.params.idProposition)
       console.log('Slug: ' + this.slug)
       console.log('Id: ' + this.idProposition)
       this.propositionResource = this.$resource('http://127.0.0.1:3000/api/teams{/slug}/propositions/{/idProposition}')
