d3a3846ae07018eb80f1b04e64394df9b48f616b
LeoMouyna <mouynaleo@gmail.com>
2017-12-13 22:29:17 +0100

    Add display for success answer

--- likvaProject/src/components/propositions/PropositionDisplay.vue
+++ likvaProject/src/components/propositions/PropositionDisplay.vue
@@ -41,8 +41,8 @@
       this.slug = this.$router.history.current.params.slug
       this.idProposition = this.$router.history.current.params.idProposition
       this.propositionResource = this.$resource('http://127.0.0.1:3000/api/teams{/slug}/propositions{/idProposition}')
-      this.propositionResource.get(
-        {slug: this.slug, idProposition: this.idProposition}).then(response => {
+      this.propositionResource.get({slug: this.slug, idProposition: this.idProposition}).then(response => {
+        console.log('Resultat de la requête: succèss = ' + response.body.success)
         this.proposition = response.body.props
         console.log('La proposition: ' + this.proposition)
         })
