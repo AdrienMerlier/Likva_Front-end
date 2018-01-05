6e613996f9f4916d03ee3d604ed7e86f09992e48
AdrienMerlier <adrien.merlier@insa-lyon.fr>
2018-01-05 14:17:47 +0100

    Verdict is now displayed

--- likvaProject/src/components/propositions/PropositionResults.vue
+++ likvaProject/src/components/propositions/PropositionResults.vue
@@ -2,7 +2,7 @@
   <div class="propositionResults">
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
-        <h2 class="display-3" id="verdict">Le résultat de cette proposition est {{result.verdict}}</h2>
+        <h2 class="display-3" id="verdict">Le résultat de cette proposition est {{verdict}}</h2>
       </div>
     </div>
   </div>
@@ -18,6 +18,7 @@
     store: userStore,
     data () {
       return {
+        verdict: {},
         results: {}
       }
     },
@@ -31,12 +32,12 @@
       ])
     },
     mounted () {
-      console.error('Je suis dans le truc de résultats.')
       this.slug = this.$router.history.current.params.slug
       this.idProposition = this.$router.history.current.params.idProposition
       this.propositionResource = this.$resource('http://127.0.0.1:3000/api/teams{/slug}/propositions{/idProposition}/results')
       this.propositionResource.get({slug: this.slug, idProposition: this.idProposition}).then(response => {
-        this.results = response.body.props[0]
+        this.verdict = response.body.verdict
+        this.results = response.body.results
       })
     }
   }
