2e1d2b2b675cff2ef4b40fff5dd891a56b763f5f
AdrienMerlier <adrien.merlier@insa-lyon.fr>
2018-01-05 17:34:19 +0100

    Getting ready to implement the donut

---
+++ likvaProject/src/components/propositions/DoughnutChart.vue
@@ -0,0 +1,20 @@
+<script>
+  import {Bar} from 'vue-chartjs'
+  // import the component - chart you need
+
+  export default Bar.extend({
+    mounted () {
+      // Overwriting base render method with actual data.
+      this.renderChart({
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [
+          {
+            label: 'News reports',
+            backgroundColor: '#3c8dbc',
+            data: [12, 20, 12, 18, 10, 6, 9, 32, 29, 19, 12, 11]
+          }
+        ]
+      },)
+    }
+  });
+</script>
