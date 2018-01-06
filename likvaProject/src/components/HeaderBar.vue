59d3873f0f6355f87a052203046cb3d04c274da8
LeoMouyna <mouynaleo@gmail.com>
2018-01-06 12:58:45 +0100

    Update HeaderBar for a better displaying

--- likvaProject/src/components/HeaderBar.vue
+++ likvaProject/src/components/HeaderBar.vue
@@ -8,11 +8,11 @@
       <!--Changer avec un router-link-->
       <a class="navbar-brand" href="#"><img src="../assets/anonymousProfile.png"/> {{userFullName}}</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse"
-              data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+              data-target="#navbarNav" aria-controls="navbarNavDropdown" aria-expanded="false"
               aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div id="navbarNavDropdown" class="navbar-collapse collapse">
+      <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <!--Changer avec un router-link-->
@@ -35,32 +35,32 @@
             <a class="nav-link" href="#" v-if="adminActualTeam">Admin</a>
           </li>
         </ul>
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownTeamSwitch"
-               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              {{actualTeamStore.displayName}}
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownTeamSwitch">
-              <a class="dropdown-item" href="#" v-for="team in userInfos.teams" v-if="userTeamsCount > 0"
-              @click.prevent="updateActualTeam(team)">{{team.displayName}}</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{name: 'home'}" class="nav-link">
-              <button type="button" class="btn btn-outline-danger nav-button" @click="logout">Déconnexion</button></router-link>
-          </li>
-        </ul>
+        <div class="d-flex flex-row justify-content-between">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownTeamSwitch"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                Mes équipes
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownTeamSwitch">
+                <a class="dropdown-item" href="#" v-for="team in userInfos.teams" v-if="userTeamsCount > 0"
+                   @click.prevent="updateActualTeam(team)">{{team.displayName}}</a>
+              </div>
+            </li>
+          </ul>
+          <router-link :to="{name: 'home'}" class="nav-item">
+            <button type="button" class="btn btn-outline-danger nav-button" @click="logout">Déconnexion</button></router-link>
+        </div>
       </div>
     </nav>
-    <nav class="navbar navbar-expand-sm navbar-dark fixed-top bg-dark" v-else>
+    <nav class="navbar navbar-expand-sm navbar-dark fixed-top bg-dark " v-else>
       <router-link :to="{name: 'home'}">
         <img src="../assets/likvaLogoMiniTransparentNavbar.png" class="navbar-brand" id="brandPicture">
       </router-link>
       <button type="button" class="btn btn-outline-info nav-button" data-toggle="modal" data-target="#loginModal">
         Connexion
       </button>
-      <button type="button" class="btn btn-outline-info nav-button" data-toggle="modal" data-target="#signinModal">
+      <button type="button" class="btn btn-outline-info nav-button " data-toggle="modal" data-target="#signinModal">
         Inscription
       </button>
     </nav>
