818cc9e15e1f23210eb23716bb264dae98231ed4
AdrienMerlier <adrien.merlier@insa-lyon.fr>
2018-01-19 14:10:20 +0100

    Managed to pass the id

--- likvaProject/src/components/teams/TeamUsersAdministration.vue
+++ likvaProject/src/components/teams/TeamUsersAdministration.vue
@@ -28,11 +28,11 @@
         <td>{{BooleanToOuiNon(user.admin)}}</td>
         <td>{{IsDelegable(user.delegable)}}</td>
         <td>
-          <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#modifyUserModal"><i class="fa fa-user-o"></i></button>
+          <button type="button" class="btn btn-warning btn-sm" @click="actualTeamUser = user._id" data-toggle="modal" data-target="#modifyUserModal"><i class="fa fa-user-o"></i></button>
           <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
-          <team-user-modifier :teamUserId="user._id"></team-user-modifier>
         </td>
       </tr>
+      <team-user-modifier :teamUserId="actualTeamUser"></team-user-modifier>
       </tbody>
     </table>
   </div>
@@ -50,6 +50,7 @@
     store: userStore,
     data () {
       return {
+        actualTeamUser: null,
         allUsers: []
       }
     },
