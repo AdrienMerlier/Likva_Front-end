b910eea199127a4cded091397f056cec2f1855d9
LeoMouyna <mouynaleo@gmail.com>
2018-01-19 22:22:59 +0100

    add a subcommentary element

--- likvaProject/src/components/commentaries/CommentaryDisplay.vue
+++ likvaProject/src/components/commentaries/CommentaryDisplay.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="commentary-display">
-		<div class="card card-body">
+		<div class="card card-body card-outline-info">
 			<div class="media">
 				<img src="../../assets/anonymousProfileLarge.png" alt="Image de Profile" class="rounded-circle"/>
 				<div class="media-body">
@@ -8,6 +8,11 @@
 					<h5 class="mt-0">{{commentary.authorDisplay}} <small class="text-muted">{{displayDate(commentary.date)}}</small></h5>
 					<!-- Display only 120 char an add read more button or a modal on commentary click ?-->
                     {{commentary.content}}
+                    <ul>
+                      <li v-for="subcommentary in commntary.subcommentaries">
+                        <subcommentary-display :subcommentary="subcommentary"></subcommentary-display>
+                      </li>
+                    </ul>
                     <subcommentary-adder></subcommentary-adder>
                 </div>
             </div>
@@ -17,10 +22,12 @@
 
 <script>
   import SubcommentaryAdder from './SubcommentaryAdder'
+  import SubcommentaryDisplay from './SubcommentaryDisplay'
   export default {
     name: 'commentary-display',
     components: [
-      SubcommentaryAdder
+      SubcommentaryAdder,
+      SubcommentaryDisplay
      ],
      props: {
       commentary: {
@@ -28,7 +35,7 @@
         default: function () {
           return {}
         }
-       {
+      }
      }
   }
 </script>
---
+++ likvaProject/src/components/commentaries/SubcommentaryDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="subcommentary-display">
+    <div class="card card-body card-outline-secondary">
+      <div class="media">
+        <img src="../../assets/anonymousProfileLarge.png" alt="Image de Profile" class="rounded-circle"/>
+        <div class="media-body">
+          <!-- Add a router link to the author -->
+          <h5 class="mt-0">{{subcommentary.authorDisplay}} <small class="text-muted">{{displayDate(subcommentary.date)}}</small></h5>
+          <!-- Display only 120 char ?-->
+          {{subcommentary.content}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'subcommentary-display',
+    props: {
+      subCommentary: {
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
