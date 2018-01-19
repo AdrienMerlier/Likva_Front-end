07cecb8ef5f62aad0cd6db17575d16a169e7a918
LeoMouyna <mouynaleo@gmail.com>
2018-01-19 14:33:24 +0100

    Change template for displaying commentaries

--- likvaProject/src/components/commentaries/CommentaryDisplay.vue
+++ likvaProject/src/components/commentaries/CommentaryDisplay.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="commentary">
+  <div class="commentary-display">
 		<div class="card card-body">
 			<div class="media">
-			  <img src="../../assets/anonymousProfileLarge.png" alt="Image de Profile" class="rounded-circle"/>
-			  <div class="media-body">
-			  	<!-- Add a router link to the author -->
-			    <h6 class="mt-0">{{commentary.authorDisplay}} <small class="text muted">{{displayDate(commentary.date)}}</small></h6>
-			    <!-- Display only 120 char an add read more button or a modal on commentary click ?-->
-			    {{commentary.content}}
-			  </div>
-			</div>
-		</div>
-	</div>
+				<img src="../../assets/anonymousProfileLarge.png" alt="Image de Profile" class="rounded-circle"/>
+				<div class="media-body">
+					<!-- Add a router link to the author -->
+					<h5 class="mt-0">{{commentary.authorDisplay}} <small class="text-muted">{{displayDate(commentary.date)}}</small></h5>
+					<!-- Display only 120 char an add read more button or a modal on commentary click ?-->
+                    {{commentary.content}}
+                    <sub-commentary-adder></sub-commentary-adder>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
