54656e513ff6a6d1ab832d96e6579211702071c9
LeoMouyna <mouynaleo@gmail.com>
2018-01-19 09:53:38 +0100

    Update template

--- likvaProject/src/components/commentaries/CommentaryDisplay.vue
+++ likvaProject/src/components/commentaries/CommentaryDisplay.vue
@@ -1,5 +1,17 @@
 <template>
-
+  <div class="commentary">
+		<div class="card card-body">
+			<div class="media">
+			  <img src="../../assets/anonymousProfileLarge.png" alt="Image de Profile" class="rounded-circle"/>
+			  <div class="media-body">
+			  	<!-- Add a router link to the author -->
+			    <h6 class="mt-0">{{commentary.authorDisplay}} <small class="text muted">{{displayDate(commentary.date)}}</small></h6>
+			    <!-- Display only 120 char an add read more button or a modal on commentary click ?-->
+			    {{commentary.content}}
+			  </div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
