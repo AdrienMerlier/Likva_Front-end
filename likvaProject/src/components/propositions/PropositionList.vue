96df8ee0d718edc965bda6c95f81ae03c009bc38
LeoMouyna <mouynaleo@gmail.com>
2018-01-20 23:59:00 +0100

    Better display for side buttons

--- likvaProject/src/components/propositions/PropositionList.vue
+++ likvaProject/src/components/propositions/PropositionList.vue
@@ -28,13 +28,13 @@
         </div>
         <div class="col-sm-2">
           <router-link :to="{name: 'category-list', params: { slug: slug }}">
-            <button type="button" class="btn btn-outline-success">Passer à l'affichage par catégorie</button>
+            <button type="button" class="btn btn-outline-success side-btn">Passer à l'affichage par catégorie</button>
           </router-link>
           <router-link :to="{name: 'team-members-list', params: { slug: slug }}">
-            <button type="button" class="btn btn-outline-info">Voir les membres de l'équipe</button>
+            <button type="button" class="btn btn-outline-info side-btn">Voir les membres de l'équipe</button>
           </router-link>
           <router-link v-if="isAdmin" :to="{name: 'team-members-administration', params: { slug: slug }}">
-            <button type="button" class="btn btn-outline-danger">Administrer les membres de l'équipes</button>
+            <button type="button" class="btn btn-outline-danger side-btn">Administrer les membres de l'équipes</button>
           </router-link>
         </div>
       </div>
@@ -106,5 +106,9 @@
     width: 80%;
     margin-left: auto;
     margin-right: auto;
+  }
+  .side-btn{
+    width: 100%;
+    margin-bottom: 5px;
   }
 </style>
