96b6831bd4148aa88ec2a3667142af47931f34bc
AdrienMerlier <adrien.merlier@insa-lyon.fr>
2018-01-20 19:11:41 +0100

    Solved indentation

--- likvaProject/src/components/propositions/PropositionList.vue
+++ likvaProject/src/components/propositions/PropositionList.vue
@@ -89,15 +89,14 @@
       this.propositionResource = this.$resource('http://127.0.0.1:3000/api/teams{/slug}/propositions')
       this.propositionResource.get({slug: this.slug}).then(response => {
           //  If server answer
-          this.allPropositions = response.body.props
-          if (this.catQuery) {
-            this.allPropositions = this.allPropositions.filter(proposition => proposition.category === this.catQuery)
-          }
-        }, _ => {
-          //  Le serveur ne répond pas
-          console.error('Le serveur semble ne pas répondre.')
+        this.allPropositions = response.body.props
+        if (this.catQuery) {
+          this.allPropositions = this.allPropositions.filter(proposition => proposition.category === this.catQuery)
         }
-      )
+      }, _ => {
+        //  Le serveur ne répond pas
+        console.error('Le serveur semble ne pas répondre.')
+      })
     }
   }
 </script>
