4491bb73b0176d8de99fa430fa755bce1274a842
LeoMouyna <mouynaleo@gmail.com>
2018-01-21 21:37:00 +0100

    Tru to watch the store

--- likvaProject/src/components/propositions/PropositionList.vue
+++ likvaProject/src/components/propositions/PropositionList.vue
@@ -87,18 +87,20 @@
       }
       )
     },
-    beforeUpdate () {
-      this.propositionResource = this.$resource('http://127.0.0.1:3000/api/teams{/slug}/propositions')
-      this.propositionResource.get({slug: this.slug}).then(response => {
+    watch:{
+      store: function () {
+        this.propositionResource = this.$resource('http://127.0.0.1:3000/api/teams{/slug}/propositions')
+        this.propositionResource.get({slug: this.slug}).then(response => {
           //  If server answer
-        this.allPropositions = response.body.props
-        if (this.catQuery) {
-          this.allPropositions = this.allPropositions.filter(proposition => proposition.category === this.catQuery)
-        }
-      }, _ => {
-        //  Le serveur ne répond pas
-        console.error('Le serveur semble ne pas répondre.')
-      })
+          this.allPropositions = response.body.props
+          if (this.catQuery) {
+            this.allPropositions = this.allPropositions.filter(proposition => proposition.category === this.catQuery)
+          }
+        }, _ => {
+          //  Le serveur ne répond pas
+          console.error('Le serveur semble ne pas répondre.')
+        })
+      }
     }
   }
 </script>
